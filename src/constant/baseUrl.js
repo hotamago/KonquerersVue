725067e83a340e643c19b06768489122ebb45554
hotamago <nhson2003@outlook.com>
2024-08-15 02:48:34 +0700

    chore: Update base URL for API calls
    The base URL for API calls has been updated to use the new domain "kon-api.hotavn.com" in the production environment. This change ensures that API requests are directed to the correct server.

--- src/constant/baseUrl.js
+++ src/constant/baseUrl.js
@@ -1,6 +1,6 @@
 // Set up axios
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://xpath-api.hotavn.com' : 'http://localhost:8010';
-// const baseURL = 'https://xpath-api.hotavn.com';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://kon-api.hotavn.com' : 'http://localhost:8010';
+// const baseURL = 'https://kon-api.hotavn.com';
 const apiVersion = 'v1.0';
 const apiPrefix = {
     "user": baseURL + "/api/" + apiVersion,
